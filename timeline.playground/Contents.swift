4b3080b8b149260851506dba3dbc9a4fc1ecd823
Malcom Jones <mjonesjr90@gmail.com>
2020-05-09 18:23:15 -0400

    add timeline

--- timeline.playground/Contents.swift
+++ timeline.playground/Contents.swift
@@ -27,7 +27,25 @@ class CircleView : UIView {
         let line = CAShapeLayer()
         line.path = path.cgPath
         line.lineWidth = 5
-        line.strokeColor = UIColor.red.cgColor
+        line.strokeColor = UIColor.white.cgColor
+        circle.inComingLine = line
+        outGoingLine = line
+        outGoingCircle = circle
+        circle.inComingCircle = self
+        return line
+    }
+    
+    func lineToWake(circle: CircleView) -> CAShapeLayer {
+        let path = UIBezierPath()
+        path.move(to: self.center)
+        path.addLine(to: circle.center)
+        
+        let line = CAShapeLayer()
+        line.path = path.cgPath
+        line.lineWidth = 5
+        line.lineDashPattern = [0.001, 10]
+        line.lineCap = CAShapeLayerLineCap.round
+        line.strokeColor = UIColor.white.cgColor
         circle.inComingLine = line
         outGoingLine = line
         outGoingCircle = circle
@@ -36,41 +54,91 @@ class CircleView : UIView {
     }
 }
 
+extension UIView {
+    func pinEdges(to other: UIView) {
+        leadingAnchor.constraint(equalTo: other.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: other.trailingAnchor).isActive = true
+        topAnchor.constraint(equalTo: other.topAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: other.bottomAnchor).isActive = true
+    }
+}
+
 class MyViewController : UIViewController {
 
-    let circle1 = CircleView(frame: CGRect(x: 100, y: 100, width: 50, height: 50))
-    let circle2 = CircleView(frame: CGRect(x: 100, y: 200, width: 50, height: 50))
-    let circle3 = CircleView(frame: CGRect(x: 100, y: 300, width: 50, height: 50))
-    let circle4 = CircleView(frame: CGRect(x: 100, y: 400, width: 50, height: 50))
+    let myView = UIView()
+    
+    let screenWidth = UIScreen.main.bounds.size.width
+    let screenHeight = UIScreen.main.bounds.size.height
+    
+    let diameter = 10
+    
+    var current = CircleView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+    var circle0 = CircleView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+    var circle1 = CircleView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+    var circle2 = CircleView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+    var circle3 = CircleView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+    var circle4 = CircleView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+    var circle5 = CircleView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
 
     override func loadView() {
-        let view = UIView()
-        view.backgroundColor = .white
-        self.view = view
+        myView.backgroundColor = .systemTeal
+        self.view = myView
+        
+        let width = screenWidth/2
+        
+        current = CircleView(frame: CGRect(x: normalize(hour: 10), y: 400, width: diameter, height: diameter))
+        
+        circle0 = CircleView(frame: CGRect(x: -(Int(diameter)), y: 400, width: diameter, height: diameter))
+        circle1 = CircleView(frame: CGRect(x: normalize(hour: 6), y: 400, width: diameter, height: diameter))
+        circle2 = CircleView(frame: CGRect(x: normalize(hour: 13), y: 400, width: diameter, height: diameter))
+        circle3 = CircleView(frame: CGRect(x: normalize(hour: 14), y: 400, width: diameter, height: diameter))
+        circle4 = CircleView(frame: CGRect(x: normalize(hour: 18), y: 400, width: diameter, height: diameter))
+        circle5 = CircleView(frame: CGRect(x: Int(width), y: 400, width: diameter, height: diameter))
 
-        circle1.backgroundColor = .red
-        view.addSubview(circle1)
+        myView.layer.addSublayer(circle0.lineTo(circle: circle1))
+        myView.layer.addSublayer(circle1.lineToWake(circle: circle2))
+        myView.layer.addSublayer(circle2.lineTo(circle: circle3))
+        myView.layer.addSublayer(circle3.lineToWake(circle: circle4))
+        myView.layer.addSublayer(circle4.lineTo(circle: circle5))
+        
+        current.backgroundColor = .purple
+        myView.addSubview(current)
+        
+        circle0.backgroundColor = .white
+        myView.addSubview(circle0)
+        
+        circle1.backgroundColor = .white
+        myView.addSubview(circle1)
 
-        circle2.backgroundColor = .red
-        view.addSubview(circle2)
+        circle2.backgroundColor = .white
+        myView.addSubview(circle2)
 
-        circle3.backgroundColor = .red
-        view.addSubview(circle3)
+        circle3.backgroundColor = .white
+        myView.addSubview(circle3)
 
-        circle4.backgroundColor = .red
-        view.addSubview(circle4)
-
-        circle1.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(didPan(gesture:))))
-
-        circle2.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(didPan(gesture:))))
-
-        circle3.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(didPan(gesture:))))
-
-        circle4.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(didPan(gesture:))))
-
-        view.layer.addSublayer(circle1.lineTo(circle: circle2))
-        view.layer.addSublayer(circle2.lineTo(circle: circle3))
-        view.layer.addSublayer(circle3.lineTo(circle: circle4))
+        circle4.backgroundColor = .white
+        myView.addSubview(circle4)
+        
+        circle5.backgroundColor = .white
+        myView.addSubview(circle5)
+    }
+    
+    func normalize(hour: Int) -> Int {
+        let width = screenWidth/2
+        return Int(width) * hour / 24
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UIView.animate(withDuration: 1, delay:0, options: [.repeat, .autoreverse], animations: {
+            self.current.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        }, completion: nil)
+    }
+    
+    override func viewDidLayoutSubviews() {
+//        let windowRect = self.view.window?.frame
+//        let windowWidth = windowRect?.size.width
     }
 
     @objc func didPan(gesture: UIPanGestureRecognizer) {
